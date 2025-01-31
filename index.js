0f5f2a59c830daf3b879fd097cf573955c098441
Hunter777 <113477952+AxieCowboy@users.noreply.github.com>
2025-01-31 11:55:19 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import 'dotenv/config'
+// import "MODEL_NAME" from './models/xyz.js'
+
+const app = express ()
+const port = 8080
+
+app.use(express.json{})
+
+mongoose.connect(process.env.MONGO_URI).then(() =>{
+    console.log("Connected to MongoDB");
+})
+
+app.get('/', (req,res) => {
+    res.send('Hello World')
+})
+
+// app.get('/listings', async (req, res) =>{
+//     console.log("hi")
+//     const listings = await Listingsandreview.find({"property_type": "Apartment"}, {"name": 1, "property_type": 1, }).limit(20)
+//     res.json(listings)
+// })
+
+app.listen(port)
